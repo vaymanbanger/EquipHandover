65ed714c3027be83e0343be8d5b173456d0750e1
Victor <vityak2019@mail.ru>
2025-08-22 21:00:58 +0300

    миграцию докинуть

---
+++ EquipHandover.Context/Migrations/20250822175821_FixedEntities.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquipHandover.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Inn",
+                table: "Senders",
+                newName: "TaxPayerId");
+
+            migrationBuilder.RenameColumn(
+                name: "Ogrn",
+                table: "Receivers",
+                newName: "RegistrationNumber");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Receiver_RegistrationNumber",
+                table: "Receivers",
+                column: "RegistrationNumber",
+                unique: true,
+                filter: "[Ogrn] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Receiver_RegistrationNumber",
+                table: "Receivers");
+
+            migrationBuilder.RenameColumn(
+                name: "TaxPayerId",
+                table: "Senders",
+                newName: "Inn");
+
+            migrationBuilder.RenameColumn(
+                name: "RegistrationNumber",
+                table: "Receivers",
+                newName: "Ogrn");
+        }
+    }
+}
